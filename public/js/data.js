7cea3484353bf69c0b17ffa54c6cc39d0408e58b
BKim <brian.kim@move.com>
2017-10-06 12:05:43 -0700

    updating range

--- public/js/data.js
+++ public/js/data.js
@@ -23,13 +23,94 @@ function getData (parameters) {
     });
 }
 
+function getCheckBox () {
+    let sList = "";
+    $('.form input[type=checkbox]').each(function () {
+        let sThisVal = (this.checked ? this.name : "0");
+        sList += (sList=="" ? sThisVal : "," + sThisVal);
+    });
+    console.log (sList);
+}
+
+function getRange() {
+    let range = $('input[name=range]:checked', '.form').val();
+    let params = {};
+    switch(range) {
+        case 'ytd':
+            params.startDate = '01/01/17';
+            params.endDate = '10/02/17';
+            break;
+        case 'py':
+            params.startDate = '11/01/16';
+            params.endDate = '10/02/17';
+            break;
+        case 'pym':
+            params.startDate = '11/01/16';
+            params.endDate = '10/02/17';
+            break;
+        case 'pm':
+            params.startDate = '11/01/16';
+            params.endDate = '10/02/17';
+            break;
+        case 'mtd':
+            params.startDate = '11/01/16';
+            params.endDate = '10/02/17';
+            break;
+        case 'pw':
+            params.startDate = '11/01/16';
+            params.endDate = '10/02/17';
+            break;
+        case 'p30d':
+            params.startDate = '11/01/16';
+            params.endDate = '10/02/17';
+            break;
+        case 'p60d':
+            params.startDate = '11/01/16';
+            params.endDate = '10/02/17';
+            break;
+        default:
+            params.startDate = '11/01/16';
+            params.endDate = '10/02/17';
+    }
+
+    return params;
+
+}
+
+function getCalData(parameters) {
+    let start = new Date(parameters.startDate);
+    let end = new Date(parameters.endDate);
+
+    return {
+        start : start.getFullYear() +  '-' + start.getMonth(),
+        end : end.getFullYear() +  '-' + end.getMonth()
+    };
+}
+
 $(function(){
     $(document).ready(function() {
-        let parameters = { startDate: '11/01/16', endDate : '10/02/17' };
+        let parameters = getRange();
 
-        $('#start').val("2016-11");
-        $('#end').val("2017-10");
+        let cal = getCalData(parameters);
+
+        $('#start').val(cal.start);
+        $('#end').val(cal.end);
 
         getData(parameters);
     });
+});
+
+
+$('.form #test1').click(function() {
+    console.log(event)
+    console.log( "Handler for .click() called." );
+
+    console.log();
+
+    getRange();
+
+    getCheckBox();
+    // $('#myForm input').on('change', function() {
+    // });
+
 });
