53227b5cd1a2dee5d00c38995d9324311dbd7360
Jacob, Jerry <Jerry.Jacob@move.com>
2017-10-06 01:13:52 -0700

    Node server demo
    Node server demo

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express   = require('express');
+var router    = express.Router();
+const service = require("./../lib/service");
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/dashboard', function (req, res, next) {
+    return service.show(req,res);
+});
+
+
+module.exports = router;
